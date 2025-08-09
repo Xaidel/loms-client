19d609b60d7199084cd16b789a1b2fdc7a4f6088
gd-salle <gerardojr.salle@gmail.com>
2025-08-09 15:06:16 +0800

    fix: dist doesnt have the courseOffering

--- dist/main.js
+++ dist/main.js
@@ -1,4 +1,5 @@
 import { uploadCurriculum } from "./relay/uploadCurriculum";
+import { uploadCourseOffering } from "./relay/uploadCourseOffering";
 export default class Client {
     BASE_URL;
     constructor(url) {
@@ -6,13 +7,22 @@ export default class Client {
     }
     Parser() {
         return {
-            /*
+            /**
              * Parse the official curriculum data from the registrar to a digestible format
              * @param curriculum file from registrar
-             * @ returns status 201
-             * */
+             * @returns status 201
+             */
             curriculum: async (csv) => {
                 const res = await uploadCurriculum(this.BASE_URL, csv);
+                return res;
+            },
+            /**
+             * Parse the course offering data
+             * @param offering file from registrar
+             * @returns status 201
+             */
+            courseOffering: async (csv) => {
+                const res = await uploadCourseOffering(this.BASE_URL, csv);
                 return res;
             }
         };
