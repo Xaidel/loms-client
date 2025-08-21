4823ab19f911aa10b7fc344266d9f4d81cf0209b
ZA005 <gerardojr.salle@gmail.com>
2025-08-21 13:47:04 +0800

    update: api url for enrolled course

--- src/relay/uploadClassList.ts
+++ src/relay/uploadClassList.ts
@@ -1,22 +1,18 @@
 import { parseClassList } from "../parser/classList";
-import Papa from "papaparse"
-import { convertToCSVFile } from "../parser/xls"
 
-export async function uploadClassList(url: string, xls: File): Promise<Record<string, unknown>> {
+export async function uploadClassList(url: string, xls: File) {
     try {
-        const csv = await convertToCSVFile(xls)
-        const data = await csv.text()
+        const data = await xls.text();
         const parsed = parseClassList(data)
-        const sanitized = Papa.unparse(parsed)
-        const sanitizedCsv = new File([sanitized], csv.name, { type: "text/csv" })
 
-        const formData = new FormData()
-        formData.append("csvFile", sanitizedCsv)
-
-        const res = await fetch(`${url}/class-list/upload`, {
+        const res = await fetch(`${url}/enrolled-courses/upload`, {
             method: "POST",
-            body: formData
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(parsed)
         })
+
         if (!res.ok) {
             const errorData = await res.json()
             throw errorData
